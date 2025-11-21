bd7d71d51d732fa31eb0ed47db3c709954c792f5
viktor <ashenarx@gmail.com>
2025-11-22 02:12:01 +1000

    Add SecurityConfig for API and actuator security setup

--- src/main/kotlin/dev/owlmajin/server/token/ServerTokenApplication.kt
+++ src/main/kotlin/dev/owlmajin/server/token/ServerTokenApplication.kt
@@ -1,9 +1,11 @@
 package dev.owlmajin.server.token
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class ServerTokenApplication
 
 fun main(args: Array<String>) {
